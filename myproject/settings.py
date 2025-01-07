79e643bd5c7510c6df92d7c6bcccb36c2bd9dce8
eugen179 <maisheugene@gmail.com>
2025-01-07 12:40:15 +0300

    change

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s)s_3dl)^46%yqt1ps#=j05(k!x7m&_sr7&_9&+qab97s-g9el
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['calorie-test.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['calorie-tests.onrender.com', 'localhost', '127.0.0.1']
 
 
 
